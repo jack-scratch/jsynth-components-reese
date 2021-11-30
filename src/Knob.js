9489e1b4a05fe05c8fa638e63e6811678ea98bbe
Jack <jackhasakeyboard@gmail.com>
2021-11-30 00:53:31 -0700

    Refactored

--- src/Knob.js
+++ src/Knob.js
@@ -1,16 +1,25 @@
 import React from 'react';
 
-const Knob = (props) => {
-	const
-		diam = props.rad * 2,
-		lineLn = 16;
+class Knob extends React.Component {
+	constructor(props) {
+		super();
 
-	return (
-		<svg className="knob raised" width={diam} height={diam} transform={"rotate(" + props.val + ")"}>
-			<circle cx={props.rad} cy={props.rad} r={props.rad} fill="#333" />
-			<line x1={props.rad} x2={props.rad} y1={diam - lineLn} y2={props.rad * 2} strokeWidth="3" stroke="grey" />
-		</svg>
-	);
+		this.rad = props.rad;
+		this.val = props.val;
+	}
+
+	render() {
+		const
+			diam = this.rad * 2,
+			lineLn = 16;
+
+		return (
+			<svg className="knob raised" width={diam} height={diam} transform={"rotate(" + this.val + ")"}>
+				<circle cx={this.rad} cy={this.rad} r={this.rad} fill="#333" />
+				<line x1={this.rad} x2={this.rad} y1={diam - lineLn} y2={this.rad * 2} strokeWidth="3" stroke="grey" />
+			</svg>
+		);
+	}
 }
 
 export default Knob;
