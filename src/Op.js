926dd615362c33b8e06ca4cf191a6d29159cf5d7
Jack <jackhasakeyboard@gmail.com>
2021-11-29 22:42:24 -0700

    Created "label" prop

--- src/Op.js
+++ src/Op.js
@@ -2,8 +2,12 @@ import './App.css';
 
 import React from 'react';
 
+import Label from './Label';
+
 const Op = (props) => {
-	return <div className="op">{props.name}</div>;
+	return <div className="op">
+		<Label text={props.name} />
+	</div>;
 }
 
 export default Op;
