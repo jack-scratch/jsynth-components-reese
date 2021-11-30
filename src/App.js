cfd689ab0c4dbd1a8ad97dd043364221ec6b8cc2
Jack <jackhasakeyboard@gmail.com>
2021-11-30 00:42:02 -0700

    Initialized audio API, played pure tone

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import Op from './Op.js';
 
 function App() {
+	// init
+	const ctx = new window.AudioContext() || window.webkitAudioContext();
+
+	// source
+	let osc = ctx.createOscillator();
+	osc.type = 'sine';
+	osc.frequency.value = 440.0;
+
+	osc.connect(ctx.destination);
+
+	// start
+	osc.start();
+
   return (
     <Op name='Operator' />
   );
