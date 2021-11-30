396d30c38377b7e8e7289c87a9303dabaf367e15
Jack <jackhasakeyboard@gmail.com>
2021-11-29 23:28:33 -0700

    Added single knob to oscillator, now diverged into head and body

---
+++ src/Knob.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Knob = (props) => {
+	const
+		rad = 50,
+		diam = rad * 2,
+
+		rot = Math.PI * 2;
+
+	let dir = true;
+
+	return (
+		<svg width="50" height="50">
+			<circle cx="25" cy="25" r="25" fill="#333" />
+			<line x1="25" x2="25" y1="34" y2="50" stroke-width="3" stroke="grey" />
+		</svg>
+	);
+}
+
+export default Knob;
--- src/Op.js
+++ src/Op.js
@@ -3,10 +3,16 @@ import './App.css';
 import React from 'react';
 
 import Label from './Label';
+import Knob from './Knob';
 
 const Op = (props) => {
 	return <div className="op">
-		<Label text={props.name} />
+		<div className="head">
+			<Label text={props.name} />
+		</div>
+		<div className="head">
+			<Knob />
+		</div>
 	</div>;
 }
 
