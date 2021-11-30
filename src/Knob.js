ae4ea77d21c92e9cd569512f739e2cc9a93f496f
Jack <jackhasakeyboard@gmail.com>
2021-11-30 00:33:50 -0700

    Referred to attribute properly

--- src/Knob.js
+++ src/Knob.js
@@ -8,7 +8,7 @@ const Knob = (props) => {
 	return (
 		<svg className="knob raised" width={diam} height={diam} transform={"rotate(" + props.val + ")"}>
 			<circle cx={props.rad} cy={props.rad} r={props.rad} fill="#333" />
-			<line x1={props.rad} x2={props.rad} y1={diam - lineLn} y2={props.rad * 2} stroke-width="3" stroke="grey" />
+			<line x1={props.rad} x2={props.rad} y1={diam - lineLn} y2={props.rad * 2} strokeWidth="3" stroke="grey" />
 		</svg>
 	);
 }
