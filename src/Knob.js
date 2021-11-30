ef61776587b77bd251f4d569e7dda8ec31d148e8
Jack <jackhasakeyboard@gmail.com>
2021-11-29 23:37:24 -0700

    Purged straggling variables in constructor

--- src/Knob.js
+++ src/Knob.js
@@ -1,14 +1,6 @@
 import React from 'react';
 
 const Knob = (props) => {
-	const
-		rad = 50,
-		diam = rad * 2,
-
-		rot = Math.PI * 2;
-
-	let dir = true;
-
 	return (
 		<svg className="knob" width="50" height="50">
 			<circle cx="25" cy="25" r="25" fill="#333" />
