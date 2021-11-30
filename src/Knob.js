7b853a3791b4294f54890d674ea05db9bf72629a
Jack <jackhasakeyboard@gmail.com>
2021-11-29 23:33:46 -0700

    Made overflow visible -- shadow no longer being clipped, made style specific to knob

--- src/Knob.js
+++ src/Knob.js
@@ -10,7 +10,7 @@ const Knob = (props) => {
 	let dir = true;
 
 	return (
-		<svg width="50" height="50">
+		<svg className="knob" width="50" height="50">
 			<circle cx="25" cy="25" r="25" fill="#333" />
 			<line x1="25" x2="25" y1="34" y2="50" stroke-width="3" stroke="grey" />
 		</svg>
