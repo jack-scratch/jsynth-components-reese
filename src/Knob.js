770ce15c549dd6433015eac528612d821fa901f3
Jack <jackhasakeyboard@gmail.com>
2021-12-02 04:36:53 -0700

    Formatted

--- src/Knob.js
+++ src/Knob.js
@@ -4,19 +4,19 @@ class Knob extends React.Component {
 	constructor(props) {
 		super(props);
 
-		this.rad = props.rad;
-		this.val = props.val;
+		this._rad = props.rad;
+		this._val = props.val;
 	}
 
 	render() {
 		const
-			diam = this.rad * 2,
+			diam = this._rad * 2,
 			lineLn = 16;
 
 		return (
-			<svg className="knob raised" width={diam} height={diam} transform={"rotate(" + this.val + ")"}>
-				<circle cx={this.rad} cy={this.rad} r={this.rad} fill="#333" />
-				<line x1={this.rad} x2={this.rad} y1={diam - lineLn} y2={this.rad * 2} strokeWidth="3" stroke="grey" />
+			<svg className="knob raised" width={diam} height={diam} transform={"rotate(" + this._val + ")"}>
+				<circle cx={this._rad} cy={this._rad} r={this._rad} fill="#333" />
+				<line x1={this._rad} x2={this._rad} y1={diam - lineLn} y2={this._rad * 2} strokeWidth="3" stroke="grey" />
 			</svg>
 		);
 	}
