cfbfe24fa3b77a6c6abe37c6570a60cd1984c33c
Jack <jackhasakeyboard@gmail.com>
2021-12-13 05:50:07 -0700

    Added module class, to encompass operator

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Op from './Op.js';
+import Module from './Module.js';
 
 function App() {
 	const ctx = new window.AudioContext() || window.webkitAudioContext();
 
   return (
-    <Op name='Operator' ctx={ctx} />
+		<div>
+			<Op name='Operator' ctx={ctx} />
+			<Module ctx={ctx} />
+		</div>
   );
 }
 
---
+++ src/Module.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+import React from 'react';
+
+class Module extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div className="module">
+				<div className="module"></div>
+			</div>
+		);
+	}
+};
+
+export default Module;
