bd4b5422d4c569f3c85269e5eb64f92d0f6bf842
Jack <jackhasakeyboard@gmail.com>
2021-12-13 14:26:06 -0700

    Refactored -- implementation of a class member which really should have been a prop

--- src/Knob.js
+++ src/Knob.js
@@ -4,32 +4,6 @@ class Knob extends React.Component {
 	constructor(props) {
 		super(props);
 
-		if (props.rad) {
-			this._rad = props.rad;
-		} else {
-			switch (props.sz) {
-				case 's':
-					this._rad = 26;
-
-					break;
-
-				case 'm':
-					this._rad = 40;
-
-					break;
-
-				case 'l':
-					this._rad = 60;
-
-					break;
-
-				default:
-					this._rad = 26;
-
-					break;
-			}
-		}
-
 		this.state = {
 			paramRef: 0
 		};
@@ -44,19 +18,42 @@ class Knob extends React.Component {
 	}
 
 	render() {
+		let rad;
+		switch (this.props.sz) {
+			case 's':
+				rad = 26;
+
+				break;
+
+			case 'm':
+				rad = 40;
+
+				break;
+
+			case 'l':
+				rad = 60;
+
+				break;
+
+			default:
+				rad = 26;
+
+				break;
+		}
+
 		const
-			diam = this._rad * 2,
+			diam = rad * 2,
 			lineLn = 16;
 
 		return (
 			<svg className="knob raised" width={diam} height={diam} transform={"rotate(" + this.state.paramRef + ")"} onClick={this.turn}>
 				<defs>
 					<clipPath id="circ">
-						<circle cx={this._rad} cy={this._rad} r={this._rad} />
+						<circle cx={rad} cy={this._rad} r={rad} />
 					</clipPath>
 				</defs>
-				<circle cx={this._rad} cy={this._rad} r={this._rad} fill="#333" />
-				<line x1={this._rad} x2={this._rad} y1={diam - lineLn} y2={this._rad * 2} strokeWidth="3" stroke="grey" clipPath="url(#circ)" />
+				<circle cx={rad} cy={rad} r={rad} fill="#333" />
+				<line x1={rad} x2={rad} y1={diam - lineLn} y2={rad * 2} strokeWidth="3" stroke="grey" clipPath="url(#circ)" />
 			</svg>
 		);
 	}
