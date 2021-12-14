9ead3499b5ce52544284f9cce9fee3eb5e79e1bb
Jack <jackhasakeyboard@gmail.com>
2021-12-14 13:26:05 -0700

    Purged contents of main component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,3 @@
-import Op from './Op';
-import Effect from './Effect';
-import Out from './Out';
-import Knob from './Knob';
-import Launch from './Launch';
-import Pulse from './Pulse';
-import Module from './Module';
-
-import './main.css';
-
 const ctx = new window.AudioContext() || window.webkitAudioContext();
 
 function App() {
@@ -20,9 +10,7 @@ function App() {
 			if (ctx.state == 'suspended') {
 				ctx.resume();
 			}
-		}}>
-			<Op ctx={ctx} />
-		</div>
+		}}></div>
   );
 }
 
