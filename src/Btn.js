c2dba41747dd5367545d99ee9589143de84b2148
Jack <jackhasakeyboard@gmail.com>
2021-12-14 11:03:14 -0700

    Tied some kind of event to button

--- src/Btn.js
+++ src/Btn.js
@@ -3,9 +3,19 @@ import React from 'react';
 import './Btn.css';
 
 class Btn extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.play = this.play.bind(this);
+	}
+
+	play() {
+		alert('asdf')
+	}
+
 	render() {
 		return (
-			<div className={'btn raised ' + this.props.sz}>
+			<div className={'btn raised ' + this.props.sz} onClick={this.play}>
 				<div>
 					<div className='mark'>{this.props.name}</div>
 				</div>
