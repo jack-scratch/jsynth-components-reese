5fb50b1f29559fd6a9ba2da99e02836da367410a
Jack <jackhasakeyboard@gmail.com>
2021-12-14 16:39:44 -0700

    Properly passed audio parameter to children in effect, added gain node

--- src/App.js
+++ src/App.js
@@ -1,3 +1,14 @@
+import Op from './Op';
+import Effect from './Effect';
+import Out from './Out';
+import Knob from './Knob';
+import Launch from './Launch';
+import Pulse from './Pulse';
+import Module from './Module';
+import Gain from './Gain';
+
+import './main.css';
+
 const ctx = new window.AudioContext() || window.webkitAudioContext();
 
 function App() {
@@ -10,7 +21,9 @@ function App() {
 			if (ctx.state == 'suspended') {
 				ctx.resume();
 			}
-		}}></div>
+		}}>
+			<Gain ctx={ctx} />
+		</div>
   );
 }
 
---
+++ src/Gain.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Effect from './Effect';
+
+class Gain extends Effect {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		let amp = this.props.ctx.createGain();
+
+		return (
+			<Effect name='Gain' knob={[
+				'Level'
+			]} paramRef={amp.gain} />
+		);
+	}
+};
+
+export default Gain;
