1a8ce189579f7a12a630a6ccf552ec13ab04626f
Jack <jackhasakeyboard@gmail.com>
2021-12-15 14:12:50 -0700

    Refactored -- utilized backticks and evaluated variables in cable

--- src/Cable.js
+++ src/Cable.js
@@ -31,7 +31,7 @@ class Cable extends React.Component {
 	render() {
 		return (
 			<svg className="cable" onClick={this.drag} onClick={this.drag}>
-				<path d={'M ' + this.state.start[0] + ' ' + this.state.start[1] + ' C ' + this.state.start[0] + ' ' + this.state.start[1] + ', ' + this.state.mid[0] + ' ' + this.state.mid[1] + ', ' + this.state.end[0] + ' ' + this.state.end[1]} />
+				<path d={`M ${this.state.start[0]},${this.state.start[1]} C ${this.state.start[0]},${this.state.start[1]} ${this.state.mid[0]},${this.state.mid[1]} ${this.state.end[0]},${this.state.end[1]}`} />
 			</svg>
 		);
 	}
