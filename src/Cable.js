77b35a6ad5e28af7f79a001b39cd499345be3509
Jack <jackhasakeyboard@gmail.com>
2021-12-15 14:09:30 -0700

    Bound some kind of event to cable, constantly fires because it constantly occupies 100% of the page

--- src/Cable.js
+++ src/Cable.js
@@ -20,11 +20,17 @@ class Cable extends React.Component {
 				100
 			]
 		};
+
+		this.drag = this.drag.bind(this);
+	}
+
+	drag() {
+		alert('asdf');
 	}
 
 	render() {
 		return (
-			<svg className="cable" onClick={this.drag}>
+			<svg className="cable" onClick={this.drag} onClick={this.drag}>
 				<path d={'M ' + this.state.start[0] + ' ' + this.state.start[1] + ' C ' + this.state.start[0] + ' ' + this.state.start[1] + ', ' + this.state.mid[0] + ' ' + this.state.mid[1] + ', ' + this.state.end[0] + ' ' + this.state.end[1]} />
 			</svg>
 		);
