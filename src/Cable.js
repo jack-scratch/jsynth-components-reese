768b05d7db86f94c7a26ac28805910f59ad227a1
Jack <jackhasakeyboard@gmail.com>
2021-12-15 13:59:36 -0700

    Implemented cable's starting and ending positions as state

--- src/Cable.js
+++ src/Cable.js
@@ -3,10 +3,25 @@ import React from "react";
 import "./Cable.css";
 
 class Cable extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			start: [
+				0,
+				0
+			],
+			end: [
+				100,
+				0
+			]
+		};
+	}
+
 	render() {
 		return (
 			<svg className="cable" onClick={this.drag}>
-				<path d={'M ' + 0 + ' ' + 0 + ' C ' + 50 + ' ' + 50 + ', ' + 100 + ' ' + 150 + ', ' + 150 + ' ' + 30} />
+				<path d={'M ' + this.state.start[0] + ' ' + this.state.start[1] + ' C ' + 50 + ' ' + 50 + ', ' + 100 + ' ' + 150 + ', ' + this.state.end[0] + ' ' + this.state.end[1]} />
 			</svg>
 		);
 	}
