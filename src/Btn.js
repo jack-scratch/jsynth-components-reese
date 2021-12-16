bf04b0177c59a38b2e14c37b853e84bb8503d892
Jack <jackhasakeyboard@gmail.com>
2021-12-16 09:39:39 -0700

    Generated buffer and played internally on button press

--- src/Btn.js
+++ src/Btn.js
@@ -7,20 +7,54 @@ class Btn extends React.Component {
 		super(props);
 
 		this.state = {
-			down: false
+			ln: 3.0,
+			buffLn: null,
+			hz: 440.0,
+			pitch: null,
+			buff: null,
+			data: null,
+			src: null,
+			down: null
 		};
+
+		this.state.buffLn = this.state.ln * this.props.ctx.sampleRate;
+		this.state.buff = this.props.ctx.createBuffer(1, this.state.buffLn, this.props.ctx.sampleRate);
+
+		this.state.down = false;
+
+		this.state.pitch = this.state.buffLn / (this.state.ln * this.state.hz);
+
+		// buffer
+		this.state.data = this.state.buff.getChannelData(0);
+
+		for (let i = 0; i < this.state.buffLn; i++) {
+			this.state.data[i] = Math.sin((i / this.state.pitch) * Math.PI * 2);
+		}
 
 		this.play = this.play.bind(this);
 		this.release = this.release.bind(this);
 	}
 
 	play() {
+		this.state.src = this.props.ctx.createBufferSource();
+		this.state.src.buffer = this.state.buff;
+
+		this.state.src.connect(this.props.ctx.destination);
+
+		this.state.src.start();
+
 		this.setState(() => ({
 			down: true
 		}));
 	}
 
 	release() {
+		if (this.state.src) {
+			this.state.src.stop();
+
+			this.state.src = null;
+		}
+
 		this.setState(() => ({
 			down: false
 		}));
