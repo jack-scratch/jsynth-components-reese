ef6bad61193c017576f520f5c5c6898434aaf5e6
Jack <jackhasakeyboard@gmail.com>
2021-12-16 07:33:49 -0700

    Compared both value and type when starting context

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ const ctx = new window.AudioContext() || window.webkitAudioContext();
 function App() {
   return (
 		<div id="app" onClick={() => {
-			if (ctx.state == "running") {
+			if (ctx.state === "running") {
 				ctx.suspend();
 			}
 
-			if (ctx.state == "suspended") {
+			if (ctx.state === "suspended") {
 				ctx.resume();
 			}
 		}}></div>
