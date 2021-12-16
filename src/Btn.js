824e28805ee6d5f0bc1f95fe9cbaa7ad277406d5
Jack <jackhasakeyboard@gmail.com>
2021-12-16 09:59:14 -0700

    Refactored

--- src/Btn.js
+++ src/Btn.js
@@ -7,9 +7,7 @@ class Btn extends React.Component {
 		super(props);
 
 		this.state = {
-			ln: 3.0,
 			buffLn: null,
-			hz: 440.0,
 			pitch: null,
 			buff: null,
 			data: null,
@@ -17,12 +15,14 @@ class Btn extends React.Component {
 			down: null
 		};
 
-		this.state.buffLn = this.state.ln * this.props.ctx.sampleRate;
+		const ln = 3.0;
+
+		this.state.buffLn = ln * this.props.ctx.sampleRate;
 		this.state.buff = this.props.ctx.createBuffer(1, this.state.buffLn, this.props.ctx.sampleRate);
 
-		this.state.down = false;
+		const hz = 440.0;
 
-		this.state.pitch = this.state.buffLn / (this.state.ln * this.state.hz);
+		this.state.pitch = this.state.buffLn / (ln * hz);
 
 		// buffer
 		this.state.data = this.state.buff.getChannelData(0);
@@ -30,6 +30,8 @@ class Btn extends React.Component {
 		for (let i = 0; i < this.state.buffLn; i++) {
 			this.state.data[i] = Math.sin((i / this.state.pitch) * Math.PI * 2);
 		}
+
+		this.state.down = false;
 
 		this.play = this.play.bind(this);
 		this.release = this.release.bind(this);
