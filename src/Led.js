f6ae3d1cfc13dd1a56cec04945e8089414eaa56a
Jack <jackhasakeyboard@gmail.com>
2021-12-16 06:52:58 -0700

    Added canvas, drew in some capacity via reference and effect hooks

--- src/Led.js
+++ src/Led.js
@@ -1,11 +1,37 @@
-import React from "react";
+import {
+	React,
+	useRef,
+	useEffect
+} from "react";
 
-class Led extends React.Component {
-	render() {
-		return (
-			<canvas></canvas>
-		);
+const Led = props => {
+	const ref = useRef(null);
+
+	let canvas;
+
+	const clear = ctx => {
+		ctx.fillStyle = "#111";
+
+		ctx.fillRect(0, 0, canvas.width, canvas.height);
 	}
+
+	const draw = ctx => {
+		ctx.fillStyle = "#f0db4f";
+
+		ctx.fillRect(0, 0, 100, 100);
+	}
+
+	useEffect(() => {
+		canvas = ref.current;
+		const ctx = canvas.getContext("2d");
+
+		clear(ctx);
+		draw(ctx);
+	}, [draw]);
+
+	return (
+		<canvas ref={ref}></canvas>
+	);
 }
 
 export default Led;
