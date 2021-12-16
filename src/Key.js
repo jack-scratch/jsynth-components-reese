477fe12c2a4e726f249d87c7059779a8c1ebb4ee
Jack <jackhasakeyboard@gmail.com>
2021-12-16 09:02:46 -0700

    Bound some kind of event to key, toggles like button

--- src/Key.js
+++ src/Key.js
@@ -7,9 +7,32 @@ import {
 import "./Key.css";
 
 class Key extends Module {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			down: false
+		};
+
+		this.play = this.play.bind(this);
+		this.release = this.release.bind(this);
+	}
+
+	play() {
+		this.setState(() => ({
+			down: true
+		}));
+	}
+
+	release() {
+		this.setState(() => ({
+			down: false
+		}));
+	}
+
 	render() {
 		return (
-			<div className="key white">
+			<div className="key white"  onMouseDown={this.play} onMouseUp={this.release} onMouseLeave={this.release}>
 				<span className="white"></span>
 			</div>
 		);
