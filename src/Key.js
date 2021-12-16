5f4c68b72989b20b8deda199d175d653d539ea9b
Jack <jackhasakeyboard@gmail.com>
2021-12-16 08:36:09 -0700

    Moved utility to generate formatted note name to "math" module

--- src/Key.js
+++ src/Key.js
@@ -6,64 +6,6 @@ import {
 
 import "./Key.css";
 
-function note(i, off) {
-	const base = 'A';
-	const sign = {
-		'sharp': '&#9839',
-		'flat': '&#9837'
-	};
-
-	const mark = {
-		'sharp': false,
-		'flat': true
-	};
-
-	const ws = ' ';
-	const sep = '/';
-
-	let c = '';
-	if (off) {
-		let relFlat = (i + 1) % oct;
-		let relSharp = i % oct;
-
-		let note = [];
-
-		if (mark['flat']) {
-			note.push(String.fromCharCode(base.charCodeAt() + relFlat) + '<sub>' + sign['flat'] + '</sub>');
-		}
-
-		if (mark['sharp']) {
-			note.push(String.fromCharCode(base.charCodeAt() + relSharp) + sign['sharp']);
-		}
-
-		let tok = [];
-		for (let i = 0; i < note.length; i++) {
-			tok.push(note[i]);
-
-			if (i < note.length - 1) {
-				tok.push(sep);
-			}
-		}
-
-		let serial = '';
-		for (let i = 0; i < tok.length; i++) {
-			serial += tok[i];
-
-			if (i < tok.length - 1) {
-				serial += ws;
-			}
-		}
-
-		c = serial;
-	} else {
-		let rel = i % oct;
-
-		c += String.fromCharCode(base.charCodeAt() + rel);
-	}
-
-	return c;
-}
-
 class Key extends Module {
 	render() {
 		return (
--- src/math.js
+++ src/math.js
@@ -2,7 +2,67 @@ const a = 440.0;
 
 const oct = 8 - 1;
 
+function note(i, off) {
+	const base = 'A';
+	const sign = {
+		'sharp': '&#9839',
+		'flat': '&#9837'
+	};
+
+	const mark = {
+		'sharp': false,
+		'flat': true
+	};
+
+	const ws = ' ';
+	const sep = '/';
+
+	let c = '';
+	if (off) {
+		let relFlat = (i + 1) % oct;
+		let relSharp = i % oct;
+
+		let note = [];
+
+		if (mark['flat']) {
+			note.push(String.fromCharCode(base.charCodeAt() + relFlat) + '<sub>' + sign['flat'] + '</sub>');
+		}
+
+		if (mark['sharp']) {
+			note.push(String.fromCharCode(base.charCodeAt() + relSharp) + sign['sharp']);
+		}
+
+		let tok = [];
+		for (let i = 0; i < note.length; i++) {
+			tok.push(note[i]);
+
+			if (i < note.length - 1) {
+				tok.push(sep);
+			}
+		}
+
+		let serial = '';
+		for (let i = 0; i < tok.length; i++) {
+			serial += tok[i];
+
+			if (i < tok.length - 1) {
+				serial += ws;
+			}
+		}
+
+		c = serial;
+	} else {
+		let rel = i % oct;
+
+		c += String.fromCharCode(base.charCodeAt() + rel);
+	}
+
+	return c;
+}
+
 export {
 	a,
-	oct
+	oct,
+
+	note
 };
