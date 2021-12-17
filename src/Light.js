0f88a6fd2fe2d98f37b4af04fcc840d17a4664e6
Jack <jackhasakeyboard@gmail.com>
2021-12-16 19:06:35 -0700

    Implemented boolean state in light

--- src/Light.js
+++ src/Light.js
@@ -3,11 +3,23 @@ import React from "react";
 import "./Light.css";
 
 class Light extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			on: this.props.on
+		};
+	}
+
 	render() {
 		return (
 			<div className={`light ${(this.props.on ? "on" : "off")}`}></div>
 		);
 	}
+};
+
+Light.defaultProps = {
+	on: false
 };
 
 export default Light;
