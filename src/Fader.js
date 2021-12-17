79de55047c4b8a98e1b9ad5ec1a5b732d04e2aed
Jack <jackhasakeyboard@gmail.com>
2021-12-16 21:06:05 -0700

    Updated position of thumb on event

--- src/Fader.js
+++ src/Fader.js
@@ -3,11 +3,29 @@ import React from "react";
 import "./Fader.css";
 
 class Fader extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			val: 0
+		};
+
+		this.drag = this.drag.bind(this);
+	}
+
+	drag() {
+		this.setState((prevState, props) => ({
+			val: prevState.val + 1
+		}));
+	}
+
 	render() {
 		return (
 			<div className="fader">
 				<div className="groove">
-					<div className="thumb">
+					<div className="thumb" style={{
+						marginTop: this.state.val
+					}} onMouseDown={this.drag}>
 						<div className="mark"></div>
 					</div>
 				</div>
