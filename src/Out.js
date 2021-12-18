a5988913c7fab2488a6b8a750122fd19f6a04bae
Jack <jackhasakeyboard@gmail.com>
2021-12-18 16:44:47 -0700

    Removed "useless constructor"

--- src/Out.js
+++ src/Out.js
@@ -2,10 +2,6 @@ import React from "react";
 import Module from "./Module";
 
 class Out extends Module {
-	constructor(props) {
-		super(props);
-	}
-
 	render() {
 		return (
 			<Module name={this.props.name} refer={this.props.refer} port={[
