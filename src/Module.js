244f75234f71b696a06d5878e07bbbe890f04d11
Jack <jackhasakeyboard@gmail.com>
2021-12-18 16:29:00 -0700

    Divied module into head and body

--- src/Module.js
+++ src/Module.js
@@ -13,26 +13,28 @@ class Module extends React.Component {
 				<div className="head">
 					<Label text={this.props.name} />
 				</div>
-				<div className="ctrl">
-					{this.props.refer ? this.props.refer.map((inst) =>
-						<div key={inst.name}>
-							<div className="head">
-								<Label text={inst.name} />
-							</div>
-							<div className="body">
-								<div className="body param">
-									<PortIn />
-									<Knob refer={inst.point} min={this.props.min} max={this.props.max} />
+				<div className="body">
+					<div className="ctrl">
+						{this.props.refer ? this.props.refer.map((inst) =>
+							<div key={inst.name}>
+								<div className="head">
+									<Label text={inst.name} />
+								</div>
+								<div className="body">
+									<div className="body param">
+										<PortIn />
+										<Knob refer={inst.point} min={this.props.min} max={this.props.max} />
+									</div>
 								</div>
 							</div>
+						) : null}
+					</div>
+					<div className="body">
+						<div className="io">
+							{this.props.port.map((inst) =>
+								inst.type === "in" ? <PortIn key={inst.type} /> : <PortOut key={inst.type} />
+							)}
 						</div>
-					) : null}
-				</div>
-				<div className="body">
-					<div className="io">
-						{this.props.port.map((inst) =>
-							inst.type === "in" ? <PortIn key={inst.type} /> : <PortOut key={inst.type} />
-						)}
 					</div>
 				</div>
 			</div>
