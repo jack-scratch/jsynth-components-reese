cad830e5b336b5ec88a139b28c0fdac30e3a0730
Jack <jackhasakeyboard@gmail.com>
2021-12-18 08:36:02 -0700

    Moved utility to generate formatted note into general "utilities" library

---
+++ src/util.js
@@ -0,0 +1,65 @@
+import {
+	oct
+} from "./math";
+
+function fmt(i, off) {
+	const base = 'A';
+	const sign = {
+		'sharp': '&#9839',
+		'flat': '&#9837'
+	};
+
+	const mark = {
+		'sharp': false,
+		'flat': true
+	};
+
+	const ws = ' ';
+	const sep = '/';
+
+	let c = '';
+	if (off) {
+		let relFlat = (i + 1) % (oct - 1);
+		let relSharp = i % (oct - 1);
+
+		let note = [];
+
+		if (mark['flat']) {
+			note.push(String.fromCharCode(base.charCodeAt() + relFlat) + '<sub>' + sign['flat'] + '</sub>');
+		}
+
+		if (mark['sharp']) {
+			note.push(String.fromCharCode(base.charCodeAt() + relSharp) + sign['sharp']);
+		}
+
+		let tok = [];
+		for (let i = 0; i < note.length; i++) {
+			tok.push(note[i]);
+
+			if (i < note.length - 1) {
+				tok.push(sep);
+			}
+		}
+
+		let serial = '';
+		for (let i = 0; i < tok.length; i++) {
+			serial += tok[i];
+
+			if (i < tok.length - 1) {
+				serial += ws;
+			}
+		}
+
+		c = serial;
+	} else {
+		let rel = i % (oct - 1);
+
+		c += String.fromCharCode(base.charCodeAt() + rel);
+	}
+
+	return c;
+}
+
+export {
+	fmt
+};
