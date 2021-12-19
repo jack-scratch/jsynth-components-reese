cd232141fe5faba3749e594f2c50682e49b902f2
Jack <jackhasakeyboard@gmail.com>
2021-12-19 10:11:02 -0700

    Added base "system" class

---
+++ src/Bay.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./Module.css";
+
+class Sys extends React.Component {
+	render() {
+		return (
+			<div className="sys">asdf</div>
+		);
+	}
+}
+
+export default Sys;
