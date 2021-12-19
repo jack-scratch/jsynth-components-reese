e57b3491e0b4b58738dd8737f93b89d1f80b27c8
Jack <jackhasakeyboard@gmail.com>
2021-12-19 11:17:13 -0700

    Added outgoing port to keyboard

--- src/Board.js
+++ src/Board.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Module from "./Module";
 import Key from "./Key";
+import PortOut from "./PortOut";
 import {
 	oct
 } from "./math";
@@ -11,28 +12,33 @@ class Board extends Module {
 	render() {
 		return (
 			<div className="board">
-				<table className="body white">
-					<tbody>
-						<tr>
-							{[...Array(this.props.ln).keys()].map((i) =>
-								<td key={`white-${i}`}>
-									<Key type="white" i={i} />
-								</td>
-							)}
-						</tr>
-					</tbody>
-				</table>
-				<table className="body black">
-					<tbody>
-						<tr>
-							{[...Array(this.props.ln).keys()].map((i) =>
-								<td key={`black-${i}`}>
-									<Key type="black" i={i} />
-								</td>
-							)}
-						</tr>
-					</tbody>
-				</table>
+				<div>
+					<table className="body white">
+						<tbody>
+							<tr>
+								{[...Array(this.props.ln).keys()].map((i) =>
+									<td key={`white-${i}`}>
+										<Key type="white" i={i} />
+									</td>
+								)}
+							</tr>
+						</tbody>
+					</table>
+					<table className="body black">
+						<tbody>
+							<tr>
+								{[...Array(this.props.ln).keys()].map((i) =>
+									<td key={`black-${i}`}>
+										<Key type="black" i={i} />
+									</td>
+								)}
+							</tr>
+						</tbody>
+					</table>
+				</div>
+				<div>
+					<PortOut />
+				</div>
 			</div>
 		);
 	}
--- src/Key.js
+++ src/Key.js
@@ -5,6 +5,7 @@ import {
 	oct,
 	trans
 } from "./math";
+import ctx from "./ctx";
 
 import "./Key.css";
 
