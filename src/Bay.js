010eb663d99ce310346a9da56cb30c92a036154a
Jack <jackhasakeyboard@gmail.com>
2021-12-19 13:46:53 -0700

    Added cables to patch bay

--- src/Bay.js
+++ src/Bay.js
@@ -1,9 +1,42 @@
 import React from "react";
+import Cable from "./Cable";
 
 class Sys extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			patch: [
+				{
+					start: [
+						30,
+						70
+					],
+					end: [
+						120,
+						300
+					]
+				}, {
+					start: [
+						120,
+						30
+					],
+					end: [
+						120,
+						300
+					]
+				}
+			]
+		};
+	}
+
 	render() {
 		return (
-			<div className="sys">asdf</div>
+			<div className="sys">
+				{this.state.patch.map((inst, i) =>
+					<Cable key={i} start={inst.start} />
+				)}
+			</div>
 		);
 	}
 }
