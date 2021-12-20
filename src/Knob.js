08ff694ba05af060228e363a8998d5773f30e192
Jack <jackhasakeyboard@gmail.com>
2021-12-20 15:51:44 -0700

    Made tick on increments of control elements general and not specific to one element

--- src/Knob.js
+++ src/Knob.js
@@ -82,7 +82,7 @@ class Knob extends React.Component {
 				)}
 
 				{shape}
-				<line x1={this.props.rad} x2={this.props.rad} y1={diam - lineLn} y2={this.props.rad * 2} />
+				<line className="tick" x1={this.props.rad} x2={this.props.rad} y1={diam - lineLn} y2={this.props.rad * 2} />
 				<text className="mark" textAnchor="end" x={-margin} y={this.props.rad}>{this.props.min}</text>
 				<text className="mark" textAnchor="start" x={diam + margin} y={this.props.rad}>{this.props.max}</text>
 			</svg>
