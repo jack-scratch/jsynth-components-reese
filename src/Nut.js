bb1f5e64838ff23a1757bd84158289c9b8fd6b1f
Jack <jackhasakeyboard@gmail.com>
2021-12-20 17:29:12 -0700

    Made nut just abstract polygon, offset currently isn't right

--- src/Nut.js
+++ src/Nut.js
@@ -1,44 +1,13 @@
 import React from "react";
+import Poly from "./Poly";
 import {
 	rotRad
 } from "./layout";
 
 class Nut extends React.Component {
 	render() {
-		const n = 3 * 2;
-
-		const stride = rotRad / n;
-
-		let pt = [];
-		for (let i = 0; i < rotRad; i++) {
-			let idx = i * stride;
-
-			pt.push([
-				Math.cos(idx),
-				Math.sin(idx)
-			]);
-		}
-
-		const hexRad = 26;
-		const portRad = 10;
-
-		let serial = "";
-		for (let i = 0; i < pt.length; i++) {
-			let op;
-			if (i) {
-				op = "L";
-			} else {
-				op = "M";
-			}
-
-			let node = op + " " + (portRad + (pt[i][0] * hexRad)) + ", " + (portRad + (pt[i][1] * hexRad))
-
-			serial += node;
-		}
-		serial += " Z";
-
 		return (
-			<path d={serial} transform={`translate(${hexRad} ${hexRad})`} />
+			<Poly n={6} rad={26} />
 		);
 	}
 }
