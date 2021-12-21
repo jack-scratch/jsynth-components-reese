2efdcc673bf545c83797b001b45bac5d5a12dfe4
Jack <jackhasakeyboard@gmail.com>
2021-12-20 20:38:35 -0700

    Added members to clear and draw to LED

--- src/Led.js
+++ src/Led.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {
+	bg,
 	js
 } from "./col";
 
@@ -8,12 +9,28 @@ class Led extends React.Component {
 		super(props);
 
 		this.canvRef = React.createRef();
+
+		this.clear = this.clear.bind(this);
+		this.draw = this.draw.bind(this);
 	}
 
 	componentDidMount() {
 		this.context = this.canvRef.current.getContext("2d");
 
+		this.clear();
+
+		this.draw();
+	}
+
+	clear() {
+		this.context.fillStyle = bg;
+
+		this.context.fillRect(0, 0, this.canvRef.current.width, this.canvRef.current.height);
+	}
+
+	draw() {
 		this.context.fillStyle = js;
+
 		this.context.fillRect(0, 0, 30, 70);
 	}
 
