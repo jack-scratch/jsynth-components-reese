242a3dcb8592d64043dd68b97706d5721158b83c
Jack <jackhasakeyboard@gmail.com>
2021-12-21 19:10:09 -0700

    Formatted -- React components reflective of being JSX, extension only needs to be specified in non-JSX index file

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import Bay from "./Bay.jsx";
+import Osc from "./Osc.jsx";
+import speaker from "./speaker.jsx";
+
+function App() {
+	window.ctx = new window.AudioContext() || window.webkitAudioContext();
+
+  return (
+		<div id="app" onClick={() => {
+			if (window.ctx.state === "running") {
+				window.ctx.suspend();
+			}
+
+			if (window.ctx.state === "suspended") {
+				window.ctx.resume();
+			}
+		}}>
+			<Bay module={[
+				<Osc />,
+				speaker()
+			]} />
+		</div>
+  );
+}
+
+export default App;
