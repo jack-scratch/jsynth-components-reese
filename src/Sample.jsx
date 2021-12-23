79ddb00a68b8d8e46214911b12b07ad63d9d761b
Jack <jackhasakeyboard@gmail.com>
2021-12-22 17:02:28 -0700

    Added navigation arrows to impulse

--- src/Sample.jsx
+++ src/Sample.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Text from "./Text";
 import Impulse from "./Impulse";
+import Btn from "./Btn";
 import {
 	FontAwesomeIcon
 } from "@fortawesome/react-fontawesome";
 import {
-	faPlay
+	faPlay,
+	faSortUp,
+	faSortDown
 } from "@fortawesome/free-solid-svg-icons";
 
 class Sample extends React.Component {
@@ -17,6 +20,10 @@ class Sample extends React.Component {
 						"asdf",
 						"hjkl"
 					]} ht={1} />
+				<div className="body">
+					<Btn name={<FontAwesomeIcon icon={faSortUp}/>} wd={40} ht={26} />
+					<Btn name={<FontAwesomeIcon icon={faSortDown}/>} wd={40} ht={26} />
+				</div>
 				</div>
 				<div className="body">
 					<Impulse name={<FontAwesomeIcon icon={faPlay}/> }/>
