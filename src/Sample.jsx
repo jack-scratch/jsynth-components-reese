404c12123b1639ca63472fc9a1d72036a1324413
Jack <jackhasakeyboard@gmail.com>
2021-12-22 18:32:04 -0700

    Formatted

--- src/Sample.jsx
+++ src/Sample.jsx
@@ -63,8 +63,8 @@ class Sample extends React.Component {
 						<Text buff={this.buff} ht={1} />
 					</div>
 					<div className="body group">
-						<Btn name={<FontAwesomeIcon icon={faSortUp}/>} wd={40} ht={26} call={this.dec} />
-						<Btn name={<FontAwesomeIcon icon={faSortDown}/>} wd={40} ht={26} call={this.inc} />
+						<Btn name={<FontAwesomeIcon icon={faSortUp} />} wd={40} ht={26} call={this.dec} />
+						<Btn name={<FontAwesomeIcon icon={faSortDown} />} wd={40} ht={26} call={this.inc} />
 					</div>
 				</div>
 				<div className="body">
