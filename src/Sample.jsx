ba8b2b965a364508ab699d1db48181b727ee51b0
Jack <jackhasakeyboard@gmail.com>
2021-12-22 17:15:21 -0700

    Made static buffer of sampler member

--- src/Sample.jsx
+++ src/Sample.jsx
@@ -15,6 +15,19 @@ class Sample extends React.Component {
 	constructor(props) {
 		super(props);
 
+		this.buff = [
+			"Bamboo",
+			"Bass-Drum-1",
+			"Bass-Drum-2",
+			"Bass-Drum-3",
+			"Boom-Kick",
+			"Bottle",
+			"Clap-1",
+			"Clap-2",
+			"Clap-3",
+			"Claves"
+		];
+
 		this.state = {
 			l: 0
 		};
@@ -24,18 +37,7 @@ class Sample extends React.Component {
 		return (
 			<div className="sys">
 				<div className="body">
-					<Text buff={[
-						"Bamboo",
-						"Bass-Drum-1",
-						"Bass-Drum-2",
-						"Bass-Drum-3",
-						"Boom-Kick",
-						"Bottle",
-						"Clap-1",
-						"Clap-2",
-						"Clap-3",
-						"Claves"
-					]} ht={1} />
+					<Text buff={this.buff} ht={1} />
 				<div className="body">
 					<Btn name={<FontAwesomeIcon icon={faSortUp}/>} wd={40} ht={26} call={() => this.setState((prevState) => ({
 						l: prevState.l - 1
