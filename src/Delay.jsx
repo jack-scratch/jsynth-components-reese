23e100ca63c637f3109b87132bde2008d31d1576
Jack <jackhasakeyboard@gmail.com>
2021-12-25 21:30:55 -0700

    Added delay module

---
+++ src/Delay.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Effect from "./Effect";
+
+class Delay extends Effect {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			time: 100.0,
+			node: window.ctx.createDelay()
+		};
+
+		this.state.node.delayTime.value = this.state.time;
+	}
+
+	render() {
+		return (
+			<Effect name={this.props.name} refer={[
+				{
+					name: "Time",
+					point: this.state.node.delayTime
+				}
+			]} min={this.props.min} max={this.props.max} />
+		);
+	}
+}
+
+Delay.defaultProps = {
+	name: "Delay"
+};
+
+export default Delay;
