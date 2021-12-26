0df37b89e83af4db25c9c579b317ae0c9a5de6e8
Jack <jackhasakeyboard@gmail.com>
2021-12-25 21:34:14 -0700

    Added panning module

---
+++ src/Pan.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Effect from "./Effect";
+
+class Pan extends Effect {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			time: 100.0,
+			node: window.ctx.createStereoPanner()
+		};
+
+		this.state.node.pan.value = this.state.time;
+	}
+
+	render() {
+		return (
+			<Effect name={this.props.name} refer={[
+				{
+					name: "Rate",
+					point: this.state.node.pan
+				}
+			]} min={this.props.min} max={this.props.max} />
+		);
+	}
+}
+
+Pan.defaultProps = {
+	name: "Pan"
+};
+
+export default Pan;
