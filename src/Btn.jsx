0c8de67da087aa064dcfd2943a6699071b1ef01a
Jack <jackhasakeyboard@gmail.com>
2021-12-27 20:25:12 -0700

    Refactored button, now takes callbacks to tie to events as props

--- src/Btn.jsx
+++ src/Btn.jsx
@@ -19,34 +19,11 @@ class Btn extends React.Component {
 			src: null,
 			down: null
 		};
-
-		this.push = this.push.bind(this);
-		this.release = this.release.bind(this);
-	}
-
-	push() {
-		this.setState({
-			down: true
-		});
-	}
-
-	release() {
-		if (this.state.src) {
-			this.state.src.stop();
-
-			this.setState({
-				src: null
-			})
-		}
-
-		this.setState({
-			down: false
-		});
 	}
 
 	render() {
 		return (
-			<div className={"btn " + (this.state.down ? " " : "raised ")} onMouseDown={this.push} onMouseUp={this.release} onMouseLeave={this.release} style={{
+			<div className={"btn " + (this.state.down ? " " : "raised ")} onMouseDown={this.props.push} onMouseUp={this.props.release} style={{
 				width: this.props.wd,
 				height: this.props.ht
 			}} onClick={this.props.call}>
