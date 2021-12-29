987d7aab156874f171014ef26e4d678836a9c296
Jack <jackhasakeyboard@gmail.com>
2021-12-28 22:46:47 -0700

    Properly passed ports throughout hierarchy

--- src/Source.jsx
+++ src/Source.jsx
@@ -4,12 +4,7 @@ import Module from "./Module";
 class Source extends Module {
 	render() {
 		return (
-			<Module name={this.props.name} refer={this.props.refer} port={[
-				{
-					type: "out",
-					point: this.props.refer
-				}
-			]} min={this.props.min} max={this.props.max} hookDown={this.props.hookDown} hookUp={this.props.hookUp} marked={this.props.marked} />
+			<Module name={this.props.name} refer={this.props.refer} port={this.props.port} min={this.props.min} max={this.props.max} hookDown={this.props.hookDown} hookUp={this.props.hookUp} marked={this.props.marked} />
 		);
 	}
 }
