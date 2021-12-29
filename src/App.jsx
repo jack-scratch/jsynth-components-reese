9a6791bd79ec30f64e457391724a558dae454c9d
Jack <jackhasakeyboard@gmail.com>
2021-12-29 08:29:16 -0700

    Added single other worklet

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,36 @@ import synth from "./synth.jsx";
 window.ctx = new window.AudioContext() || window.webkitAudioContext();
 
 function App() {
-	window.ctx.audioWorklet.addModule("worklet/noise.js").then(() => {
-		let node = new AudioWorkletNode(window.ctx, "white");
+	window.ctx.audioWorklet.addModule("worklet/dsp.js").then(() => {
+		// source
+		const oscillator = window.ctx.createOscillator();
+
+		oscillator.type = "sine";
+		oscillator.frequency.value = 5000.0;
+
+		// effect
+		const bitCrusherNode = new AudioWorkletNode(window.ctx, "bitcrush");
+
+		const paramBitDepth = bitCrusherNode.parameters.get("bitDepth");
+		paramBitDepth.setValueAtTime(1.0, 0.0);
+
+		const paramReduction = bitCrusherNode.parameters.get("frequencyReduction");
+
+		const beginning = window.ctx.currentTime;
+		const middle = window.ctx.currentTime + 4;
+		const end = window.ctx.currentTime + 8;
+
+		// schedule
+		paramReduction.setValueAtTime(0.01, beginning);
+		paramReduction.linearRampToValueAtTime(0.1, middle);
+		paramReduction.exponentialRampToValueAtTime(0.01, end);
 
 		// route
-		node.connect(window.ctx.destination);
+		oscillator.connect(bitCrusherNode).connect(window.ctx.destination);
+
+		// start
+		oscillator.start();
+		oscillator.stop(end);
 	});
 
   return (
