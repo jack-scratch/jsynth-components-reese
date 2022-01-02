80ea26f86d8bd05329fabfe48316451115766112
Jack <jackhasakeyboard@gmail.com>
2022-01-02 09:00:26 -0700

    Added chorus module, extends source because current implementation of effect has a static one input and one output

---
+++ src/Chorus.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import Source from "./Source";
+
+class Osc extends Source {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			node: window.ctx.createOscillator(),
+			hz: 0.0
+		};
+
+		if (this.props.type) {
+			this.state.node.type = this.props.type;
+		}
+
+		this.state.node.start();
+	}
+
+	componentDidMount() {
+		this.setState({
+			hz: this.state.node.frequency.value
+		});
+	}
+
+	render() {
+		return (
+			<Source name={this.props.name} min={this.props.min} max={this.props.max} node={[
+				{
+					name: "Spread",
+					point: this.state.node.frequency
+				}
+			]} port={[
+				{
+					type: "out",
+					point: this.state.node
+				}, {
+					type: "out",
+					point: this.state.node
+				}, {
+					type: "out",
+					point: this.state.node
+				}, {
+					type: "out",
+					point: this.state.node
+				}, {
+					type: "out",
+					point: this.state.node
+				}, {
+					type: "out",
+					point: this.state.node
+				}
+			]} hookDown={this.props.hookDown} marked />
+		);
+	}
+}
+
+Osc.defaultProps = {
+	name: "Chorus",
+	min: 1.0,
+	max: 100.0
+};
+
+export default Osc;
