d88fac5ac09781293d102488c86a9f5d03410dd0
Jack <jackhasakeyboard@gmail.com>
2022-01-03 06:11:04 -0700

    Added ADSR envelope, nothing implemented internally

---
+++ src/Env.jsx
@@ -0,0 +1,39 @@
+import Effect from "./Effect.jsx";
+
+class Env extends Effect {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			node: window.ctx.createBiquadFilter()
+		};
+
+		this.state.node.type = this.props.type;
+	}
+
+	render() {
+		return (
+			<Effect name={this.props.name} node={[
+				{
+					name: "Attack",
+					point: this.state.node.frequency
+				}, {
+					name: "Decay",
+					point: this.state.node.frequency
+				}, {
+					name: "Sustain",
+					point: this.state.node.frequency
+				}, {
+					name: "Release",
+					point: this.state.node.frequency
+				}
+			]} min={this.props.min} max={this.props.max} />
+		);
+	}
+}
+
+Env.defaultProps = {
+	name: "Envelope"
+};
+
+export default Env;
