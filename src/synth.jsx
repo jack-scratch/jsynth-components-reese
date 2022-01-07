971b5659f6faecb3d1d2a87eb3c337052ed15344
Jack <jackhasakeyboard@gmail.com>
2022-01-07 16:05:36 -0700

    Padded sections of patch bay

--- src/synth.jsx
+++ src/synth.jsx
@@ -13,33 +13,39 @@ const synth = () => {
 		<Bay body={[
 			<div>
 				<div className="head">
-					<h1>Synth</h1>
-				</div>
-				<div className="head">
-					<Label text="Bank" />
-				</div>
-				<div className="body row">
-					<Osc name="Sine" type="sine" />
-					<Osc name="Square" type="square" />
-					<Osc name="Sawtooth" type="sawtooth" />
-					<Osc name="Triangle" type="triangle" />
-				</div>
-				</div>, <div>
-					<div className="head">
-						<Label text="Filter" />
-					</div>
-					<div className="body row">
-						<Lowpass />
-						<Highpass />
-					</div>
-				</div>, <div>
-					<div className="head">
-						<Label text="Output" />
-					</div>
 					<div className="body">
-						{speaker()}
+						<h1>Synth</h1>
 					</div>
 				</div>
+				<div className="body">
+					<div>
+						<div className="head">
+							<Label text="Bank" />
+						</div>
+						<div className="body row">
+							<Osc name="Sine" type="sine" />
+							<Osc name="Square" type="square" />
+							<Osc name="Sawtooth" type="sawtooth" />
+							<Osc name="Triangle" type="triangle" />
+						</div>
+					</div>, <div>
+						<div className="head">
+							<Label text="Filter" />
+						</div>
+						<div className="body row">
+							<Lowpass />
+							<Highpass />
+						</div>
+					</div>, <div>
+						<div className="head">
+							<Label text="Output" />
+						</div>
+						<div className="body">
+							{speaker()}
+						</div>
+					</div>
+				</div>
+			</div>
 		]} />
 	);
 }
