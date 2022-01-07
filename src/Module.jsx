d7d80123f3314abb562963b5ea8aa5a2bde697d6
Jack <jackhasakeyboard@gmail.com>
2022-01-06 18:19:44 -0700

    Allowed for a lack of a minimum and maximum on parameter props, defaulting to the built-in ones

--- src/Module.jsx
+++ src/Module.jsx
@@ -28,7 +28,7 @@ class Module extends React.Component {
 								<div className="body">
 									<div className="body param">
 										<In node={this.props.node} hookDown={this.props.hookDown} hookEnter={this.props.hookEnter} />
-										<Knob node={inst.point} min={inst.min} max={inst.max} marked={this.props.marked} unit={unit["freq"]} quant={inst.quant ? inst.quant : null} />
+										<Knob node={inst.point} min={inst.min ? inst.min : inst.point.minValue} max={inst.max ? inst.max : inst.point.maxValue} marked={this.props.marked} unit={unit["freq"]} quant={inst.quant ? inst.quant : null} />
 									</div>
 								</div>
 							</div>
