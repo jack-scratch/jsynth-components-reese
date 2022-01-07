ff0d77e9667e4f9d44040da4763d1763a805965e
Jack <jackhasakeyboard@gmail.com>
2022-01-07 08:49:00 -0700

    Accessed previous state object when updating regardless of whether the key is the same

--- src/Knob.jsx
+++ src/Knob.jsx
@@ -31,7 +31,7 @@ class Knob extends React.Component {
 		this.setState((prevState) => this.setState({
 			startY: e.nativeEvent.clientY,
 			currY: e.nativeEvent.clientY,
-			prevVal: this.state.val,
+			prevVal: prevState.val,
 			down: true
 		}));
 	}
