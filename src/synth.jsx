15b976b584d1de9a3869ad7d37b815e24db46a3d
Jack <jackhasakeyboard@gmail.com>
2022-01-08 08:52:58 -0700

    Refactored synth into multiple different patch bays

--- src/synth.jsx
+++ src/synth.jsx
@@ -10,45 +10,47 @@ import speaker from "./speaker";
 
 const synth = () => {
 	return (
-		<Bay module={[
-			<div>
-				<div className="head">
-					<div className="body">
-						<h1>Synth</h1>
+		<div className="sys">
+			<div className="head">
+				<h1>Synth</h1>
+			</div>
+			<div className="body">
+				<div>
+					<div className="head">
+						<Label text="Bank" />
+					</div>
+					<div className="body row">
+						<Bay module={[
+							<Osc name="Sine" type="sine" />,
+							<Osc name="Square" type="square" />,
+							<Osc name="Sawtooth" type="sawtooth" />,
+							<Osc name="Triangle" type="triangle" />
+						]} />
 					</div>
 				</div>
-				<div className="body">
-					<div>
-						<div className="head">
-							<Label text="Bank" />
-						</div>
-						<div className="body row">
-							<Osc name="Sine" type="sine" />
-							<Osc name="Square" type="square" />
-							<Osc name="Sawtooth" type="sawtooth" />
-							<Osc name="Triangle" type="triangle" />
-						</div>
+				<div>
+					<div className="head">
+						<Label text="Filter" />
 					</div>
-					<div>
-						<div className="head">
-							<Label text="Filter" />
-						</div>
-						<div className="body row">
-							<Lowpass />
+					<div className="body row">
+						<Bay module={[
+							<Lowpass />,
 							<Highpass />
-						</div>
+						]} />
 					</div>
-					<div>
-						<div className="head">
-							<Label text="Output" />
-						</div>
-						<div className="body">
-							{speaker()}
-						</div>
+				</div>
+				<div>
+					<div className="head">
+						<Label text="Output" />
+					</div>
+					<div className="body row">
+						<Bay module={[
+							speaker()
+						]} />
 					</div>
 				</div>
 			</div>
-		]} />
+		</div>
 	);
 }
 
