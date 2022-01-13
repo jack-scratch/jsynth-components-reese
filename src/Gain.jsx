aed929ff21c5d4cceecab8945eb5687b5a67da12
Jack <jackhasakeyboard@gmail.com>
2022-01-12 17:50:08 -0700

    Converted state to built-in member

--- src/Gain.jsx
+++ src/Gain.jsx
@@ -4,11 +4,9 @@ class Gain extends Effect {
 	constructor(props) {
 		super(props);
 
-		this.state = {
-			node: window.ctx.createGain()
-		};
+		this.node = window.ctx.createGain();
 
-		this.state.node.gain.value = this.props.level;
+		this.node.gain.value = this.props.level;
 	}
 
 	render() {
@@ -16,7 +14,7 @@ class Gain extends Effect {
 			<Effect name="Gain" param={[
 				{
 					name: "Level",
-					point: this.state.node.gain,
+					point: this.node.gain,
 					min: -12.0,
 					max: 12.0
 				}
