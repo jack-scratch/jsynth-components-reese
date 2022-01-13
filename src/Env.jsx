4d5b487842d8ee032aac384c0e946b9004c49f33
Jack <jackhasakeyboard@gmail.com>
2022-01-12 17:21:58 -0700

    Converted state to built-in member

--- src/Env.jsx
+++ src/Env.jsx
@@ -4,12 +4,10 @@ class Env extends Effect {
 	constructor(props) {
 		super(props);
 
-		this.state = {
-			node: window.ctx.createBiquadFilter()
-		};
+		this.node = window.ctx.createBiquadFilter();
 
 		if (this.props.type) {
-			this.state.node.type = this.props.type;
+			this.node.type = this.props.type;
 		}
 	}
 
@@ -18,16 +16,16 @@ class Env extends Effect {
 			<Effect name={this.props.name} param={[
 				{
 					name: "Attack",
-					point: this.state.node.frequency
+					point: this.node.frequency
 				}, {
 					name: "Decay",
-					point: this.state.node.frequency
+					point: this.node.frequency
 				}, {
 					name: "Sustain",
-					point: this.state.node.frequency
+					point: this.node.frequency
 				}, {
 					name: "Release",
-					point: this.state.node.frequency
+					point: this.node.frequency
 				}
 			]} />
 		);
