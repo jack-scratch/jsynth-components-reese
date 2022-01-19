b97332b757b8e9a74a38e3b1a225704e961df886
Jack <jackhasakeyboard@gmail.com>
2022-01-18 21:30:05 -0700

    Implemented spectrum analyzer

--- src/Analyzer.jsx
+++ src/Analyzer.jsx
@@ -12,14 +12,6 @@ class Analyzer extends LCD {
 
 		this.clear = this.clear.bind(this);
 
-		this.src = window.ctx.createOscillator();
-
-		// route
-		this.src.connect(window.ctx.destination);
-
-		// start
-		this.src.start();
-
 		this.draw = this.draw.bind(this);
 	}
 
@@ -27,25 +19,54 @@ class Analyzer extends LCD {
 		this.refer.current.width = this.props.wd;
 		this.refer.current.height = this.props.ht;
 
-		window.ctx = this.refer.current.getContext("2d");
+		this.src = window.ctx.createOscillator();
 
-		window.requestAnimationFrame(this.draw);
+		const sz = Math.pow(2, 10);
+
+		this.analyser = window.ctx.createAnalyser();
+		this.proc = window.ctx.createScriptProcessor(sz, 1, 1);
+
+		this.ctxCanv = this.refer.current.getContext("2d");
+
+		this.analyser.fftSize = sz;
+
+		this.data = new Uint8Array(this.analyser.frequencyBinCount);
+
+		// route
+		this.src.connect(this.analyser);
+
+		this.analyser.connect(this.proc);
+		this.proc.connect(window.ctx.destination);
+
+		this.src.connect(window.ctx.destination);
+
+		// start
+		this.src.start();
+
+		this.proc.onaudioprocess = this.draw;
 	}
 
 	clear() {
-		window.ctx.fillStyle = light["inert"];
+		this.ctxCanv.fillStyle = light["inert"];
 
-		window.ctx.fillRect(0, 0, this.refer.current.width, this.refer.current.height);
+		this.ctxCanv.fillRect(0, 0, this.refer.current.width, this.refer.current.height);
 	}
 
 	draw() {
 		this.clear();
 
-		window.ctx.fillStyle = light["active"];
+		this.ctxCanv.fillStyle = light["active"];
 
-		window.ctx.fillRect(0, 0, 50, 50);
-
-		window.requestAnimationFrame(this.draw);
+		this.analyser.getByteFrequencyData(this.data);
+		for (let x = 0; x < this.refer.current.width; x++) {
+			if (x < this.data.length) {
+				for (let y = 0; y < this.refer.current.height; y++) {
+					if (y < this.data[x] / 2) {
+						this.ctxCanv.fillRect(x, this.refer.current.height - y, 1, 1);
+					}
+				}
+			}
+		}
 	}
 
 	render() {
