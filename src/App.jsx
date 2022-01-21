f630d8022ecfbd2a1379193e46b655b4a37b4c8f
Jack <jackhasakeyboard@gmail.com>
2022-01-20 19:33:39 -0700

    Removed suspending of audio before immediately resuming, which was essentially causing a "pop" on any kind of mouse down event

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,6 @@ window.ctx = new window.AudioContext() || window.webkitAudioContext();
 function App() {
   return (
 		<div id="app" onMouseDown={() => {
-			if (window.ctx.state === "running") {
-				window.ctx.suspend();
-			}
-
 			if (window.ctx.state === "suspended") {
 				window.ctx.resume();
 			}
