52b24c0feaf79bb26637af0f1dd55dab236ef7d8
Jack <jackhasakeyboard@gmail.com>
2022-01-21 00:30:02 -0700

    Properly passed all port properties in effect, allowing for a variable number of inputs and outputs

--- src/Effect.jsx
+++ src/Effect.jsx
@@ -3,15 +3,7 @@ import Module from "./Module";
 class Effect extends Module {
 	render() {
 		return (
-			<Module name={this.props.name} param={this.props.param} port={[
-				{
-					type: "in",
-					point: this.props.point
-				}, {
-					type: "out",
-					point: this.props.point
-				}
-			]} hookInDown={this.props.hookInDown} hookInUp={this.props.hookInUp} hookOutDown={this.props.hookOutDown} marked={this.props.marked} />
+			<Module name={this.props.name} param={this.props.param} port={this.props.port} hookInDown={this.props.hookInDown} hookInUp={this.props.hookInUp} hookOutDown={this.props.hookOutDown} marked={this.props.marked} />
 		);
 	}
 }
