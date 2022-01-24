4ce97261bcb23edeef6081056335fc8160219556
Jack <jackhasakeyboard@gmail.com>
2022-01-23 19:00:16 -0700

    Merged the scaffolding of the test worklet into the main module, reset main component to default state

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,6 @@
-import React from "react";
-import Module from "./Module";
-import Bay from "./Bay";
-import Osc from "./Osc";
-import speaker from "./speaker";
-import Text from "./Text";
-import Bitcrush from "./Bitcrush";
-
 import "./main.css";
 
 window.ctx = new window.AudioContext() || window.webkitAudioContext();
-
-class Worklet extends Module {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			param: {}
-		};
-	}
-
-	async componentDidMount() {
-		await window.ctx.audioWorklet.addModule(`worklet/${this.props.fName}.js`);
-
-		this.node = new AudioWorkletNode(window.ctx, this.props.workName);
-
-		for (let key of this.node.parameters.keys()) {
-			this.state.param[key] = null;
-		}
-
-		let i = 0;
-		for (let val of this.node.parameters.values()) {
-			let key = Object.keys(this.state.param)[i];
-
-			this.state.param[key] = {
-				min: val.minValue,
-				max: val.maxValue
-			};
-
-			i++;
-		}
-
-		this.setState({
-		});
-
-		// route
-		this.node.connect(window.ctx.destination);
-	}
-
-	render() {
-		return (
-			<Module name={this.props.workName} param={Object.keys(this.state.param).map((key) => (
-				{
-					name: key,
-					point: this.node,
-					min: this.state.param[key].min,
-					max: this.state.param[key].max
-				}
-			))} port={[
-				{
-					type: "in",
-					point: this.node.main
-				}, {
-					type: "out",
-					point: this.node.main
-				}
-			]} />
-		);
-	}
-}
 
 function App() {
   return (
@@ -75,13 +8,7 @@ function App() {
 			if (window.ctx.state === "suspended") {
 				window.ctx.resume();
 			}
-		}}>
-			<Bay>
-				<Osc />
-				{speaker()}
-				<Worklet fName="dsp" workName="sin" />
-			</Bay>
-		</div>
+		}}></div>
   );
 }
 
--- src/Worklet.jsx
+++ src/Worklet.jsx
@@ -2,34 +2,61 @@ import Module from "./Module";
 
 class Worklet extends Module {
 	constructor(props) {
-		super();
+		super(props);
 
-		this.param = {};
+		this.state = {
+			param: {}
+		};
 	}
 
 	async componentDidMount() {
-		window.ctx.audioWorklet.addModule(`worklet/{this.props.fName}.js`).then(() => {
-			this.node = new AudioWorkletNode(window.ctx, this.props.fName);
+		await window.ctx.audioWorklet.addModule(`worklet/${this.props.fName}.js`);
+
+		this.node = new AudioWorkletNode(window.ctx, this.props.workName);
+
+		for (let key of this.node.parameters.keys()) {
+			this.state.param[key] = null;
+		}
+
+		let i = 0;
+		for (let val of this.node.parameters.values()) {
+			let key = Object.keys(this.state.param)[i];
+
+			this.state.param[key] = {
+				min: val.minValue,
+				max: val.maxValue
+			};
+
+			i++;
+		}
+
+		this.setState({
 		});
+
+		// route
+		this.node.connect(window.ctx.destination);
 	}
 
 	render() {
 		return (
-			<Module name={this.props.fName} param={this.node.parameters.map((el) => ({
-				name: this.props.fName,
-				value: el
-			}))} port={[
+			<Module name={this.props.workName} param={Object.keys(this.state.param).map((key) => (
 				{
-					type: "out",
-					point: this.node
+					name: key,
+					point: this.node,
+					min: this.state.param[key].min,
+					max: this.state.param[key].max
 				}
-			]} hookOutDown={this.props.hookOutDown} />
+			))} port={[
+				{
+					type: "in",
+					point: this.node.main
+				}, {
+					type: "out",
+					point: this.node.main
+				}
+			]} />
 		);
 	}
 }
-
-Worklet.defaultProps = {
-	name: "Oscillator"
-};
 
 export default Worklet;
