2455151ac938f027e9b0c8670af0cdcb08dffbbe
Jack <jackhasakeyboard@gmail.com>
2022-01-27 05:45:21 -0700

    Added text display to operator, the first instance of a child not just rendering a module component

--- src/Op.jsx
+++ src/Op.jsx
@@ -1,4 +1,5 @@
 import Source from "./Source";
+import Text from "./Text";
 
 class Op extends Source {
 	constructor(props) {
@@ -22,24 +23,31 @@ class Op extends Source {
 
 	render() {
 		return (
-			<Source name={this.props.name} param={[
-				{
-					name: "Frequency",
-					point: this.node.osc.frequency,
-					min: this.props.rngFreq[0],
-					max: this.props.rngFreq[1]
-				}, {
-					name: "Gain",
-					point: this.node.amp.gain,
-					min: this.props.rngVol[0],
-					max: this.props.rngVol[1]
-				}
-			]} port={[
-				{
-					type: "out",
-					point: this.node.amp
-				}
-			]} hookInUp={this.props.hookInUp} hookOutDown={this.props.hookOutDown} />
+			<div className="module">
+				<div>
+					<Text buff={[
+						"asdf"
+					]} wd={10} ht={1} />
+				</div>
+				<Source name={this.props.name} param={[
+					{
+						name: "Frequency",
+						point: this.node.osc.frequency,
+						min: this.props.rngFreq[0],
+						max: this.props.rngFreq[1]
+					}, {
+						name: "Gain",
+						point: this.node.amp.gain,
+						min: this.props.rngVol[0],
+						max: this.props.rngVol[1]
+					}
+				]} port={[
+					{
+						type: "out",
+						point: this.node.amp
+					}
+				]} hookInUp={this.props.hookInUp} hookOutDown={this.props.hookOutDown} />
+			</div>
 		);
 	}
 }
