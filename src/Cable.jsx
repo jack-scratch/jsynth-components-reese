31e37743342ca142b0abd9d286551fb6b8d9c73c
Jack <jackhasakeyboard@gmail.com>
2022-01-28 06:15:31 -0700

    Rectified calculation of port radius

--- src/Cable.jsx
+++ src/Cable.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {
+	nutRad,
 	portRad
 } from "./layout";
 
@@ -15,15 +16,15 @@ class Cable extends React.Component {
 
 	midX() {
 		let start = [
-			this.props.inRefer.current.offsetLeft + 26 + portRad,
-			this.props.inRefer.current.offsetTop + 26 + portRad
+			this.props.inRefer.current.offsetLeft + nutRad + portRad,
+			this.props.inRefer.current.offsetTop + nutRad + portRad
 		];
 
 		let end = [];
 		if (this.props.outPoint) {
 			end = [
-				this.props.outRefer.current.offsetLeft + 26 + portRad,
-				this.props.outRefer.current.offsetTop + 26 + portRad
+				this.props.outRefer.current.offsetLeft + nutRad + portRad,
+				this.props.outRefer.current.offsetTop + nutRad + portRad
 			];
 		} else {
 			end = this.props.curr;
@@ -38,8 +39,8 @@ class Cable extends React.Component {
 		let start = [];
 		if (this.props.inPoint) {
 			start = [
-				this.props.inRefer.current.offsetLeft + 26 + portRad,
-				this.props.inRefer.current.offsetTop + 26 + portRad
+				this.props.inRefer.current.offsetLeft + nutRad + portRad,
+				this.props.inRefer.current.offsetTop + nutRad + portRad
 			];
 		} else {
 			start = this.props.curr;
@@ -48,8 +49,8 @@ class Cable extends React.Component {
 		let end = [];
 		if (this.props.outPoint) {
 			end = [
-				this.props.outRefer.current.offsetLeft + 26 + portRad,
-				this.props.outRefer.current.offsetTop + 26 + portRad
+				this.props.outRefer.current.offsetLeft + nutRad + portRad,
+				this.props.outRefer.current.offsetTop + nutRad + portRad
 			];
 		} else {
 			end = this.props.curr;
@@ -62,8 +63,8 @@ class Cable extends React.Component {
 		let start = [];
 		if (this.props.inPoint) {
 			start = [
-				this.props.inRefer.current.offsetLeft + 26 + portRad,
-				this.props.inRefer.current.offsetTop + 26 + portRad
+				this.props.inRefer.current.offsetLeft + nutRad + portRad,
+				this.props.inRefer.current.offsetTop + nutRad + portRad
 			];
 		} else {
 			start = this.props.curr;
@@ -72,8 +73,8 @@ class Cable extends React.Component {
 		let end = [];
 		if (this.props.outPoint) {
 			end = [
-				this.props.outRefer.current.offsetLeft + 26 + portRad,
-				this.props.outRefer.current.offsetTop + 26 + portRad
+				this.props.outRefer.current.offsetLeft + nutRad + portRad,
+				this.props.outRefer.current.offsetTop + nutRad + portRad
 			];
 		} else {
 			end = this.props.curr;
