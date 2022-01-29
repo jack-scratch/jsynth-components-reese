2d027f7cbeaf060823cae2d4d4084e4dcd3f3886
Jack <jackhasakeyboard@gmail.com>
2022-01-29 10:11:59 -0700

    Formatted

--- src/Btn.jsx
+++ src/Btn.jsx
@@ -49,7 +49,7 @@ class Btn extends React.Component {
 			<div className={"btn" + (this.state.down ? "" : " raised")} onMouseDown={this.push} onMouseUp={this.release} onMouseLeave={this.release} style={{
 				width: this.props.wd,
 				height: this.props.ht
-			}} onMouseDown={this.props.hookDown} onMouseUp={this.props.hookUp} onMouseLeave={this.props.hookUp}>
+			}} onMouseDown={this.props.hookOn} onMouseUp={this.props.hookOff} onMouseLeave={this.props.hookOff}>
 				<div>
 					<div className="mark">{this.props.name}</div>
 				</div>
