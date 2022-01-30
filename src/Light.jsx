6bf442d0f37bb2435fc7d934f238ab126e10a093
Jack <jackhasakeyboard@gmail.com>
2022-01-30 01:29:19 -0700

    Removed totally arbitrary state from component

--- src/Light.jsx
+++ src/Light.jsx
@@ -3,14 +3,6 @@ import React from "react";
 import "./Light.css";
 
 class Light extends React.Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			on: this.props.on
-		};
-	}
-
 	render() {
 		return (
 			<div className={`light ${(this.props.on ? "on" : "off")}`}></div>
