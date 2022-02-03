9b7adc5e9bc3b342789de9fc5d374ae026ccd5b9
Jack <jackhasakeyboard@gmail.com>
2022-02-02 17:22:27 -0700

    Added example that just adds a control-element and a display to show the value

---
+++ src/Disp.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Knob from "./Knob";
+import Text from "./Text";
+
+class Disp extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.update = this.update.bind(this);
+
+		this.state = {
+			val: 0
+		};
+	}
+
+	update(val) {
+		this.setState({
+			val: val
+		});
+	}
+
+	render() {
+		return (
+			<div className="sys">
+				<div className="head">
+					<h1>Display</h1>
+				</div>
+				<div className="body">
+					<Text buff={[
+						this.state.val
+					]} wd={8} />
+				</div>
+				<div className="body">
+					<div className="ctrl">
+						<Knob hook={this.update} min={0.0} max={100.0} />
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Disp;
