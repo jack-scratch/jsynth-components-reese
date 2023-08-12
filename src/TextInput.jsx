22dc2d8ebedfedd517d524417917b8279e0f4767
JackHasaKeyboard <jackhasakeyboard@gmail.com>
2023-08-12 01:08:13 -0600

    Removed functionless constructor

--- src/TextInput.jsx
+++ src/TextInput.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 
 class TextInput extends React.Component {
-	constructor(props) {
-		super(props);
-	}
-
 	render() {
 		return (
 			<textarea cols={this.props.wd} rows={this.props.ln} spellCheck="false"></textarea>
