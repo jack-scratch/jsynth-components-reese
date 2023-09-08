69724632c72a80e6d2768f90efe2bd36706a1650
JackHasaKeyboard <jackhasakeyboard@gmail.com>
2023-09-08 01:26:27 -0600

    Styled switch

--- src/Switch.jsx
+++ src/Switch.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import {
+	inert,
+	bg
+} from "./col";
 
 class Switch extends React.Component {
 	constructor(props) {
@@ -11,7 +15,22 @@ class Switch extends React.Component {
 
 	render() {
 		return (
-			<div className="body"></div>
+			<div className="cont">
+				<div id="track" style={{
+					height: 10,
+					width: 2 * 10,
+					background: inert,
+					cursor: "pointer"
+				}}>
+					<div id="nub" style={{
+						position: "absolute",
+						height: 10,
+						width: 10,
+						background: bg,
+						marginLeft: this.state.on * 10
+					}}></div>
+				</div>
+			</div>
 		);
 	}
 }
