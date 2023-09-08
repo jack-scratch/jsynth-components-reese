2a585499242720d583c423e1c97065e9a30e9efe
JackHasaKeyboard <jackhasakeyboard@gmail.com>
2023-09-07 18:45:25 -0600

    Added base unimplemented switch as a control component

---
+++ src/Switch.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+class Switch extends React.Component {
+	constructor(props) {
+		super();
+
+		this.state = {
+			on: false
+		};
+	}
+
+	render() {
+		return (
+			<div className="body"></div>
+		);
+	}
+}
+
+export default Switch;
