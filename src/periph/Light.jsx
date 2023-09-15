8317e18360e5bbef77d26809ab2721d10ec257b8
JackHasaKeyboard <jackhasakeyboard@gmail.com>
2023-09-15 16:56:21 -0600

    Updated implementation of light to take a generic numeric value, updates the "brightness" when a normalized value and gets clamped to being a solid light when the value exceeds the normalized range, now effectively handles numeric JavaScript values and doesn't imitate a boolean integer

--- src/periph/Light.jsx
+++ src/periph/Light.jsx
@@ -1,19 +1,27 @@
 import React from "react";
+import {
+	bg,
+	js
+} from "../col";
+import {
+	clamp
+} from "../util";
 
 import "./Light.css";
 
 class Light extends React.Component {
 	render() {
 		return (
-			<svg className={`light ${(this.props.data ? "on" : "off")}`}>
-				<circle />
+			<svg className="light">
+				<circle fill={bg} />
+				<circle fill={js} opacity={clamp(this.props.data, 0.0, 1.0)} />
 			</svg>
 		);
 	}
 }
 
 Light.defaultProps = {
-	data: false
+	data: 0.0
 };
 
 export default Light;
